c970f557b4a2db79ffa661e94b52757ac0c6db0b
wata0722nabe <wata0722nabe@icloud.com>
2020-07-22 05:17:03 +0900

    Add select function

--- index.js
+++ index.js
@@ -17,10 +17,11 @@ const weatherData = {
         { day: 'Sat', temp: 17, windDirection: 'south-east', windSpeed: 20, type: 'cloudy' },
     ]
 }
+
 for (let day of weatherData.days) {
     let data = document.getElementById(day.day);
     if (dayName.includes(day.day)) {
-        data.className = 'active'
+        data.className = 'active';
         let precipitation = document.getElementById('precipitation');
         precipitation.getElementsByClassName('value')[0].innerHTML = day.windDirection;
         let humidity = document.getElementById('humidity');
@@ -33,4 +34,26 @@ for (let day of weatherData.days) {
         document.getElementById('date-day').innerHTML = `${year}-${month}-${today.getDate()}`;
     }
     data.innerHTML = `<i class="day-icon" data-feather="cloud"></i><span class="day-name">${day.day}</span><span class="day-temp">${day.temp}°C</span>`;
+}
+
+function selectDay(selectedDay) {
+    for (let day of weatherData.days) {
+        let data = document.getElementById(day.day);
+        if (selectedDay === day.day) {
+            data.className = 'active';
+            let precipitation = document.getElementById('precipitation');
+            precipitation.getElementsByClassName('value')[0].innerHTML = day.windDirection;
+            let humidity = document.getElementById('humidity');
+            humidity.getElementsByClassName('value')[0].innerHTML = day.type;
+            let wind = document.getElementById('wind');
+            wind.getElementsByClassName('value')[0].innerHTML = `${day.windSpeed} km/h`;
+            document.getElementById('weather-desc').innerHTML = day.type;
+            document.getElementById('weather-temp').innerHTML = `${day.temp}°C`;
+            document.getElementById('date-dayname').innerHTML = dayName;
+            document.getElementById('date-day').innerHTML = `${year}-${month}-${today.getDate()}`;
+        } else {
+            data.classList.remove('active');
+        }
+        data.innerHTML = `<i class="day-icon" data-feather="cloud"></i><span class="day-name">${day.day}</span><span class="day-temp">${day.temp}°C</span>`;
+    }
 }
