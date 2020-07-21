15dc424ef455c5295ca8a78df21502608c95f73d
wata0722nabe <wata0722nabe@icloud.com>
2020-07-22 05:19:21 +0900

    Update weatherData

--- index.js
+++ index.js
@@ -12,9 +12,9 @@ const weatherData = {
         { day: 'Mon', temp: 22, windDirection: 'north-east', windSpeed: 10 , type:'sunny' },
         { day: 'Tue', temp: 14, windDirection: 'north-west', windSpeed: 14, type: 'rainy' },
         { day: 'Wed', temp: 17, windDirection: 'south-east', windSpeed: 20, type: 'cloudy' },
-        { day: 'Thu', temp: 17, windDirection: 'south-east', windSpeed: 20, type: 'cloudy' },
-        { day: 'Fri', temp: 17, windDirection: 'south-east', windSpeed: 20, type: 'cloudy' },
-        { day: 'Sat', temp: 17, windDirection: 'south-east', windSpeed: 20, type: 'cloudy' },
+        { day: 'Thu', temp: 18, windDirection: 'south-east', windSpeed: 25, type: 'sunny' },
+        { day: 'Fri', temp: 22, windDirection: 'south-east', windSpeed: 20, type: 'sunny' },
+        { day: 'Sat', temp: 7, windDirection: 'south-east', windSpeed: 20, type: 'cloudy' },
     ]
 }
 
